9af613d503978d5b572f941601112f85e3c47ba8
José Humberto Lopes Vasques <jose.vasques@dataprev.gov.br>
2025-09-10 10:19:42 -0300

    LeetCode Problem 14 - Longest Common Prefix

---
+++ src/main/java/com/study/leetcode/Problem14.java
@@ -0,0 +1,21 @@
+package com.study.leetcode;
+
+// 14. Longest Common Prefix
+public class Problem14 {
+    public static void main(String[] args) {
+        System.out.println("output: " + longestCommonPrefix(new String[]{"flower","flow","flight"}));
+        System.out.println("output: " + longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int indexOf = strs[i].indexOf(prefix);
+            while (indexOf != 0 ) {
+                prefix = prefix.substring(0, prefix.length() - 1); // Quando beginIndex = endIndex no substring o retorno eh ""
+                indexOf = strs[i].indexOf(prefix);
+            }
+        }
+        return prefix;
+    }
+}
