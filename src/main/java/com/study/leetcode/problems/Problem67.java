8ab21e71fe9de21b8348e633f4b6d5af3fb668c3
Humberto Vasques <humbertovasques.dev@gmail.com>
2025-12-12 11:20:45 -0300

    feat: update Problem67 to optimize addBinary with carry handling

--- src/main/java/com/study/leetcode/problems/Problem67.java
+++ src/main/java/com/study/leetcode/problems/Problem67.java
@@ -28,6 +28,29 @@ package com.study.leetcode.problems;
 public class Problem67 {
     public String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
+    }
+
+    public String addBinaryV1(String a, String b) {
+        StringBuilder result = new StringBuilder();
 
         String s1;
         String s2;
