0927426ca4d038fc459fd8fe02ac610a040ee458
Humberto Vasques <humbertovasques.dev@gmail.com>
2025-12-18 11:45:43 -0300

    feat: add utility methods buildList and toArray in ListNode

--- src/main/java/com/study/leetcode/common/ListNode.java
+++ src/main/java/com/study/leetcode/common/ListNode.java
@@ -1,5 +1,8 @@
 package com.study.leetcode.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListNode {
 
     public int val;
@@ -19,5 +22,34 @@ public class ListNode {
     @Override
     public String toString() {
         return val + " -> " + next;
+    }
+
+    public static ListNode buildList(int[] values) {
+        ListNode head = null;
+        ListNode tail = null;
+
+        for (int v : values) {
+            ListNode node = new ListNode(v);
+            if (head == null) {
+                head = node;
+                tail = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        return head;
+    }
+
+    // Converte uma lista encadeada em um array para facilitar o assert
+    public static int[] toArray(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            list.add(current.val);
+            current = current.next;
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
