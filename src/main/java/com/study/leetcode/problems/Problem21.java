33c4ab482071cdf76d2926ea9a3a61ccacca4e04
Humberto Vasques <humbertovasques.dev@gmail.com>
2025-12-21 12:55:38 -0300

    feat: add solution for Problem21 with implementation and examples

--- src/main/java/com/study/leetcode/problems/Problem21.java
+++ src/main/java/com/study/leetcode/problems/Problem21.java
@@ -1,3 +1,57 @@
+//
+// Merge the two lists into one sorted list. The list should be made by
+//splicing together the nodes of the first two lists.
+//
+// Return the head of the merged linked list.
+//
+//
+// Example 1:
+//
+//
+//Input: list1 = [1,2,4], list2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+//
+//
+// Example 2:
+//
+//
+//Input: list1 = [], list2 = []
+//Output: []
+//
+//
+// Example 3:
+//
+//
+//Input: list1 = [], list2 = [0]
+//Output: [0]
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+//
+//
+// Related Topics Linked List Recursion 👍 24426 👎 2400
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+//import com.study.leetcode.common.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 package com.study.leetcode.problems;
 
 import com.study.leetcode.common.ListNode;
@@ -5,6 +59,26 @@ import com.study.leetcode.common.ListNode;
 public class Problem21 {
 
     ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode tail = dummyHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = list1 != null ? list1 : list2;
+
+        return dummyHead.next;
+    }
+
+    ListNode mergeTwoListsV1(ListNode list1, ListNode list2) {
         ListNode p1 = list1;
         ListNode p2 = list2;
         ListNode dummy = new ListNode(-1);
@@ -15,7 +89,6 @@ public class Problem21 {
                 tail.next = p1;
                 tail = tail.next;
                 p1 = p1.next;
-
             } else {
                 tail.next = p2;
                 tail = tail.next;
